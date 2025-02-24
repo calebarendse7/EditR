9ca3bc112a33a47f770eb78e5aab54f928a9bb21
Caleb Arendse <arenc02@gmail.com>
2025-02-23 23:00:35 -0500

    Changed ui back to mudblazor.

--- EditR/Program.cs
+++ EditR/Program.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using EditR;
-using Radzen;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddRadzenComponents();
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
